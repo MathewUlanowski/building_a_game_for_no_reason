aa84b735b38f531852ec779cdfdf8566efc89fb5
ApTR4t <imwd.dcfranks@protonmail.com>
2022-05-14 19:52:25 -0500

    ADD - Class to Draw Circle

--- Models/Circle_Model.py
+++ Models/Circle_Model.py
@@ -1,18 +1,30 @@
-from matplotlib.backend_bases import LocationEvent
 import pygame
+
+from Models.Screen_Model import Screen
 
 
 class Circle:
 
-    def __init__(self,size,color:tuple):
-        self.__baseSetup(size,color)
-    def __init__(self,size,color:tuple,location:tuple):
-        self.__baseSetup(location)
+    def __init__(self,size,color:tuple,location:tuple,__defaultSurface):
+        self.__baseSetup(size,color,location,__defaultSurface)
+        
 
-    def __baseSetup(self,size,color:tuple,location:tuple):
+    def __baseSetup(self,size,color:tuple,location:tuple,__defaultSurface):
         self.Size = size
         self.Color = color
         self.Location = location
+        self.__DefaultSurface = __defaultSurface
+        
+    def Display(self):
+        pygame.draw.circle(self.__DefaultSurface,self.Color,self.Location,self.Size)
 
-    def moveTo(self, moveX:tuple,moveY:tuple):
+    def MoveTo(self, moveX:tuple,moveY:tuple):
         self.Move = pygame.Rect.move(self,moveX,moveY)
+
+
+    def Move_Y(self,moveY):
+        self.Location[1] = self.Location[1] + moveY
+    def Move_X(self,moveX):
+        self.Location[0] = self.Location[0] + moveX
+
+    
